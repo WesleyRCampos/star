e4f3928078bd3ae43312998faa7dd6e6fb343694
wesley de campos rodrigues <wesleyc21rodrigues@gmail.com>
2023-12-13 10:47:34 -0300

    v1.0.1

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = ({ characters }) => {
         <div className="card" key={character.name}>
           <div className="card-body">
             <h5 className="card-title"> {character.name}</h5>
-           
+            
             <p className="card-text">Altura: {character.height}</p>
             <p className="card-text">Peso: {character.mass}</p>
             <p className="card-text">Cor: {character.skin_color}</p>
